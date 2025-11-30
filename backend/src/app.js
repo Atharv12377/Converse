b07ffff201d0bf0d190631985516459deab02b21
Atharv Nayak <atharvnayak10@gmail.com>
2025-11-30 19:18:58 +0530

    Did the package.json thing in the root to add script for sevilla to build and run the app, then in the app.js fixed the catch all route  error using regex route instead of "*" in express 5

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,25 @@
-import express from "express"
-import dotenv from "dotenv"
-dotenv.config()
-const app  = express()
-const PORT = process.env.PORT
-import authRouter from "./route/auth.route.js"
-import messageRouter from "./route/message.route.js"
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+import authRouter from "./route/auth.route.js";
+import messageRouter from "./route/message.route.js";
 
-app.use("/api/auth", authRouter)
-app.use("/api/messages", messageRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/messages", messageRouter);
 
-//this is the main file
+//this is the main file, we are also serving the frontend thru the backend while deploying, because we cannot deploy fronted and backend twice, we serve frontend thru backend and also serve backend. So we have only the backend to deploy
 
-
-app.listen(PORT, ()=>{
-    console.log("The server is running on port "+ PORT)
-})
-  
+//Here we make it ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); //this here means that  when we are ready for deployment we use the dist folder that is the bundled frontend folder as static assets.
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+app.listen(PORT, () => {
+  console.log("The server is running on port " + PORT);
+});
